2af7589b19d22267741733d4384b79c222bbfd02
Nguyen Manh Hung <nguyenmanhhung20184@gmail.com>
2025-04-11 08:58:50 +0700

    push

--- code/main.cpp
+++ code/main.cpp
@@ -2,25 +2,24 @@
 using namespace std;
 
 const int MAXN = 1e5 + 1;
-
-int d[MAXN]; // kc
-int pre[MAXN]; // diem truoc
-bool vis[MAXN]; // den tham
-vector<int> g[MAXN]; // ds ke ( dinh - ke )
+int dis[MAXN];
+int pre[MAXN];
+bool vis[MAXN];
+vector<int> ds[MAXN];
 
 int n;
-int st, ed; // dau, cuoi
+int diem_dau, diem_ket_thuc; // dau, cuoi
 
-struct BFSLog {
+struct bfs_log {
     char current;
     string neighbors;
     string queueState;
 };
 
-vector<BFSLog> logs;
+vector<bfs_log> logs;
 
 void read_input() {
-    ifstream file("D:/Nam3-Ky2/_TriTueNhanTao/BTL/BFS/input.txt");
+    ifstream file("D:/Nam3-Ky2/_TriTueNhanTao/BTL/BFS/res/input.txt");
     if (!file.is_open()) {
         cout << "Khong mo duoc file" << endl;
         return;
@@ -36,37 +35,38 @@ void read_input() {
         int diemDau = lines[i][0] - 'A';
         for (int j = 1; j < lines[i].size(); j++) {
             if (lines[i][j] == ' ' || lines[i][j] == '\n') continue;
-            g[diemDau].push_back(lines[i][j] - 'A');
+            ds[diemDau].push_back(lines[i][j] - 'A');
         }
     }
 
-    st = lines[n-1][0] - 'A';
-    ed = lines[n-1][2] - 'A';
+    diem_dau = lines[n-1][0] - 'A';
+    diem_ket_thuc = lines[n-1][2] - 'A';
 }
 
 void bfs(int s) {
-    memset(vis, false, sizeof(vis));
-    memset(d, 0, sizeof(d));
-    memset(pre, -1, sizeof(pre));
     queue<int> q;
     q.push(s);
     vis[s] = true;
-
+    bfs_log log;
+    log.current = ' ';
+    log.neighbors = "";
+    log.queueState += char(q.front() + 'A');
+    logs.push_back(log);
     while (!q.empty()) {
-        BFSLog log;
+        bfs_log log;
         int u = q.front();
         log.current = char(u + 'A');
         q.pop();
-        if (u == ed) {
+        if (u == diem_ket_thuc) {
             log.neighbors = "TTKT-DUNG";
             logs.push_back(log);
             return;
         }
-        for (auto v : g[u]) {
+        for (auto v : ds[u]) {
             log.neighbors += char(v + 'A');
             log.neighbors += ',';
             if (!vis[v]) {
-                d[v] = d[u] + 1;
+                dis[v] = dis[u] + 1;
                 pre[v] = u;
                 q.push(v);
                 vis[v] = true;
@@ -84,55 +84,55 @@ void bfs(int s) {
     }
 }
 
-void write_output(int e, string file_name = "D:/Nam3-Ky2/_TriTueNhanTao/BTL/BFS/output.txt") {
-    ofstream file(file_name);
+void write_output(int ket_thuc) {
+    ofstream file("D:/Nam3-Ky2/_TriTueNhanTao/BTL/BFS/res/output.txt");
     if (!file) {
-        cerr << "Khong the mo file " << file_name << endl;
+        cerr << "Khong the mo file " << endl;
         return;
     }
     file << string(82, '-') << endl;
     file << "| " << left << setw(25) << "Phat trien trang thai"
          << "| " << setw(25) << "Trang thai ke"
-         << "| " << setw(25) << "Danh sach" << "|\n";
+         << "| " << setw(25) << "Danh sach" << "|" << endl;
     file << string(82, '-') << endl;
 
     for (auto log : logs) {
         file << "| " << left << setw(25) << log.current
              << "| " << setw(25) << log.neighbors
              << "| " << setw(25) << log.queueState << "|\n";
-        file << string(82, '-') << endl;
     }
-
-    if (!vis[e]) {
+    file << string(82, '-') << endl;
+    if (!vis[ket_thuc]) {
         file << "Khong co duong di" << endl;
-    } else {
+    }
+    else {
         vector<char> path;
-        for (int i = pre[e]; i != -1; i = pre[i]) {
+        for (int i = pre[ket_thuc]; i != -1; i = pre[i]) {
             path.push_back('A' + i);
         }
         reverse(path.begin(), path.end());
         file << "Duong di: ";
         for (auto x : path)
             file << x << " -> ";
-        file << char('A' + e) << endl;
+        file << char('A' + ket_thuc) << endl;
     }
     file.close();
-    cout << "Da in ra file " + file_name << endl;
+    cout << "Chay thanh cong" << endl;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    memset(vis, false, sizeof(vis));
+    memset(dis, 0, sizeof(dis));
+    memset(pre, -1, sizeof(pre));
     read_input();
     for (int i = 0; i < n; i++) {
         cout << "Dinh " << char(i + 'A') << " ke voi: ";
-        for (auto x : g[i]) {
+        for (auto x : ds[i]) {
             cout << char(x + 'A')  << ' ';
         }
         cout << endl;
     }
-    cout << "Diem dau: " << char(st + 'A') << ' ' << "Diem cuoi: " << char(ed + 'A') << endl;
-    bfs(st);
-
-    write_output(ed);
+    cout << "Diem dau: " << char(diem_dau + 'A') << ' ' << "Diem cuoi: " << char(diem_ket_thuc + 'A') << endl;
+    bfs(diem_dau);
+    write_output(diem_ket_thuc);
 }
